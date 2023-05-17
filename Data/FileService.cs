8d117c40ddc536c33de2f2eb12e344e27262632f
kubbit98 <kuba98pawlowicz@gmail.com>
2023-05-17 23:39:27 +0200

    fix #2 add functionality for new folders

--- Data/FileService.cs
+++ Data/FileService.cs
@@ -43,41 +43,12 @@ namespace Sorter.Data
             LoadFilesAndFolders();
             indexOfActualProcessingFile = -1;
         }
-        //public Dictionary<string, string> Destinations = new Dictionary<string, string>();
-        //public int FileNumber = 0;
-
-        /*public Task<File[]> GetFilesAsync()
-        {
-            return Task.FromResult(Files.ToArray());
-        }*/
         public Task<Folder[]> GetFoldersAsync()
         {
             return Task.FromResult(Folders.ToArray());
         }
-        /*public File GetFile()
-        {
-            File f = Files[findex];
-            f.Path = f.PhysicalPath;
-            if (f.Path.Contains(source)) f.Path = f.Path.Replace(source, "/src");
-            else if (f.Path.Contains(destination)) f.Path = f.Path.Replace(destination, "/dest");
-            f.Path = f.Path.Replace("\\", "/");
-            f.FIndex= findex;
-            return f;
-        }*/
-        /*public File GetNextFile()
-        {
-            if (findex < Files.Count - 1) ++findex;
-            return GetFile();
-        }*/
         public int? GetNextIndex()
         {
-            /*if (findex < Files.Count - 1)
-            {
-                //++findex;
-                return ++findex;
-
-            }
-            return null;*/
             return (indexOfActualProcessingFile < Files.Count - 1) ? ++indexOfActualProcessingFile : null;
         }
         public File? GetFileAtIndex(int index)
@@ -99,13 +70,6 @@ namespace Sorter.Data
             if (file == null) return null;
             if (!file.Name.Equals(name)) throw new Exception("You need to reload yor session, the server was reset");
             return file;
-            /*if (index < Files.Count && index >= 0)//-1?
-            {
-                File f = Files[index];
-                if (!f.Name.Equals(name)) throw new Exception("You need to reload yor session, the server was reset");
-                return PrepareFileToGet(f,index);
-            }*/
-            //return null;
         }
         public void ResetFiles()
         {
@@ -130,11 +94,6 @@ namespace Sorter.Data
             LoadFilesAndFolders();
             indexOfActualProcessingFile = -1;
         }
-        /*public File GetPreviousFile()
-        {
-            if (findex > 0) --findex;
-            return GetFile();
-        }*/
         private void LoadFilesAndFolders()
         {
             try
@@ -146,7 +105,6 @@ namespace Sorter.Data
             {
                 Console.WriteLine(e.Message);
             }
-
         }
         private List<Folder> ProcessDirectoryRecursively(string targetDirectoryPath)
         {
@@ -174,22 +132,11 @@ namespace Sorter.Data
             List<File> files = new List<File>();
             foreach (var fullPathToFile in Directory.GetFiles(sourceDirectoryPath))
             {
-                /*try
-                {*/
                 var fileName = Path.GetFileName(fullPathToFile);
                 var fileExtension = Path.GetExtension(fullPathToFile).Replace(".", "").ToLower();
-                //var pathToFileSplitTab = fullPathToFile.Split(Path.DirectorySeparatorChar);
-                //var fileName = pathToFileSplitTab[pathToFileSplitTab.Length - 1];
-                //var fileExtension = fileName.Split('.').Last().ToLower();
                 if ((UseWhiteListInsteadOfBlackList && !whiteList.Contains(fileExtension))
                     || (!UseWhiteListInsteadOfBlackList && blackList.Contains(fileExtension))) continue;
                 files.Add(new File(fullPathToFile, fileName, fileExtension));
-                //Console.WriteLine(findex2++);
-                //}
-                /*catch (Exception e)
-                {
-                    Console.WriteLine("Error with file " + fullPathToFile + "\n" + e.Message);
-                }*/
             }
             foreach (var fullPath in Directory.GetDirectories(sourceDirectoryPath))
             {
@@ -215,15 +162,25 @@ namespace Sorter.Data
                     file.PhysicalPath = Path.Combine(destiny, file.Name);
                     Files[file.FIndex.Value].PhysicalPath = file.PhysicalPath;
                 }
-                else
-                {
-                    Console.WriteLine("The file index has changed, you need to reload session");
-                }
+                else Console.WriteLine("The file index has changed, you need to reload session");
             }
             catch (Exception e)
             {
                 Console.WriteLine("Cannot move file\n" + e.Message);
             }
+        }
+        public bool CreateFolder(string folderName)
+        {
+            if (Folders.Any(f => f.Name == folderName)) return false;
+            string fullPath = Path.Combine(destination, folderName);
+            Directory.CreateDirectory(fullPath);
+            folderName = Path.GetFileName(fullPath);
+            if(folderName != null)
+            {
+                Folders.Add(new Folder(fullPath, folderName));
+                return true;
+            }
+            return false;
         }
     }
 }
