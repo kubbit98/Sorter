fe1f6a04d23f1107e1bfe7e3d1e72d6b62645e66
kubbit98 <kuba98pawlowicz@gmail.com>
2023-05-28 19:57:57 +0200

    add build.cmd file to speed up packing the app in zips

---
+++ build.cmd
@@ -0,0 +1,23 @@
+SET name=%1
+IF [%name%] == [] (GOTO Prompt) ELSE (GOTO NEXT)
+:Prompt
+SET /p "name=Enter name of deploy: "
+
+:Next
+SET dirr=bin\Prod\net6.0\publish
+
+dotnet publish sorter.sln -p:PublishProfile=Win64 -c Release
+dotnet publish sorter.sln -p:PublishProfile=Linux64 -c Release
+
+IF EXIST %dirr%\%name%-win64 rmdir /S /Q %dirr%\%name%-win64
+ren %dirr%\win64 %name%-win64
+IF EXIST %dirr%\%name%-linux64 rmdir /S /Q %dirr%\%name%-linux64
+ren %dirr%\linux64 %name%-linux64
+
+IF EXIST %dirr%\%name%-win64\config.json DEL /F %dirr%\%name%-win64\config.json
+IF EXIST %dirr%\%name%-linux64\config.json DEL /F %dirr%\%name%-linux64\config.json
+
+IF EXIST %dirr%\%name%-win64.zip DEL /F %dirr%\%name%-win64.zip
+IF EXIST %dirr%\%name%-linux64.tar DEL /F %dirr%\%name%-linux64.tar
+"C:\Program Files\7-Zip\7z.exe" a %dirr%\%name%-win64.zip .\%dirr%\%name%-win64\*
+"C:\Program Files\7-Zip\7z.exe" a %dirr%\%name%-linux64.tar .\%dirr%\%name%-linux64\*
